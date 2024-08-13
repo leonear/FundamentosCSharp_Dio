a41df53d3bcd374aec2b92a839ca9ffc74c725df
leonear <lsilvaaparicio@gmail.com>
2024-08-13 10:00:42 -0300

    uso do conceito de namespace

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Projeto1.Models;//Verifica o caminho lógico para a classe criada
+Pessoa p = new Pessoa();
