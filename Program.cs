4a8b5f9d3e0fe4945e95c9294f7c822e15568bb4
leonear <lsilvaaparicio@gmail.com>
2024-08-13 11:03:44 -0300

    Testando Tipos de variaveis

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
 ﻿using Projeto1.Models;//Verifica o caminho lógico para a classe criada
-Pessoa pessoa1 = new Pessoa();//p é a variável, pode ter qualquer nome
+
+string apresentacao = "Ola, seja bem vindo";
+
+int quantidade = 1;
+
+double altura = 1.80;
+
+decimal preco = 1.80M;//por estar declarando um valor diretamente a variavel decimal pede esse "m" no final
+
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variavel quantidade: " + quantidade);
+Console.WriteLine("Valor da variavel altura: " + altura);
+Console.WriteLine("Valor da variavel preco: " + preco);
+Console.WriteLine("Valor da variavel condicao: " + condicao);
+
+
+// Pessoa pessoa1 = new Pessoa();//p é a variável, pode ter qualquer nome
 
 pessoa1.Nome = "Leo";
-pessoa1.Idade = 20;
-pessoa1.Apresentar();//método
+// pessoa1.Idade = 20;
+// pessoa1.Apresentar();//método
