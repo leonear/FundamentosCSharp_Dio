26543ec2c954be2bfb9c2c68227623a2b7ca6fb8
leonear <lsilvaaparicio@gmail.com>
2024-08-15 20:43:33 -0300

    Operadores Condicionais

--- Program.cs
+++ Program.cs
@@ -3,6 +3,28 @@
 
 
 
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque} ");
+Console.WriteLine($"Quantidade de Compra: {quantidadeCompra} ");
+Console.WriteLine($"É Possível realizar a venda? {possivelVenda}");
+
+if (possivelVenda)
+{
+    Console.WriteLine("Venda Realizada.");
+}
+else
+{
+    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.d");
+}
+
+
+
+
+
 
 
 
