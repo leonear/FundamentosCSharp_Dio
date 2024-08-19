52b9d4b277170e72c977aa063461960944b154d5
leonear <lsilvaaparicio@gmail.com>
2024-08-19 18:19:43 -0300

    Ajuste

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -7,14 +7,14 @@ namespace Projeto1.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; } // método
+        public required string Nome { get; set; } // método
         public int Idade { get; set; }// método
 
         public void Apresentar()// método
         {
-            //Console.WriteLine($"Olá, meu nome é {Nome} " +
-            //$" e tenho {Idade} anos");
-            Console.WriteLine($"Olá, meu nome é {Nome} \n e tenho {Idade} anos"); //diferente formar de quebrar linha.
+            Console.WriteLine($"Olá, meu nome é {Nome} " +
+            $" e tenho {Idade} anos");
+            //Console.WriteLine($"Olá, meu nome é {Nome} \n e tenho {Idade} anos"); //diferente formar de quebrar linha.
         }
     }
 
