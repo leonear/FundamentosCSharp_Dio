740e04592cccfca17f872033f7edad57555146ba
leonear <lsilvaaparicio@gmail.com>
2024-08-19 18:16:04 -0300

    Operadores Aritméticos e a classe Math

---
+++ Models/Calculadora.cs
@@ -0,0 +1,62 @@
+﻿namespace Projeto1.Models;
+
+public class Calculadora
+{
+    public void Somar(int x, int y)
+    {
+        Console.WriteLine($"{x} + {y} = {x + y}");
+    }
+
+    public void Subtrair(int x, int y)
+    {
+        Console.WriteLine($"{x} - {y} = {x - y}");
+    }
+
+    public void Multiplicar(int x, int y)
+    {
+        Console.WriteLine($"{x} X {y} = {x * y}");
+    }
+
+    public void Dividir(int x, int y)
+    {
+        Console.WriteLine($"{x} / {y} = {x / y}");
+    }
+
+    public void Potencia(int x, int y)
+    {
+        Console.WriteLine($"{x}^{y} = {Math.Pow(x, y)}");
+    }
+
+    public void Seno(double angulo)
+    {
+        double radiano = angulo * Math.PI / 180;
+        double seno = Math.Sin(radiano);
+        Console.WriteLine($"Seno de {angulo}º = {Math.Round(seno,4)}");
+        
+    }
+    
+    public void Coseno(double angulo)
+    {
+        double radiano = angulo * Math.PI / 180;
+        double coseno = Math.Cos(radiano);
+        Console.WriteLine($"Coseno de {angulo}º = {Math.Round(coseno,4)}");
+        
+    }
+    
+    public void Tangente(double angulo)
+    {
+        double radiano = angulo * Math.PI / 180;
+        double tangente = Math.Tan(radiano);
+        Console.WriteLine($"Tangente de {angulo}º = {Math.Round(tangente,4)}");
+        
+    }
+
+
+    public void RaizQuadrada(double x)
+    {
+        double raiz = Math.Sqrt(x);
+        Console.WriteLine($"Raiz Quadrada de {x} = {raiz}");
+    }
+    
+
+}
